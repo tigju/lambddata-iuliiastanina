5edcb18ae4239aa80f4719107294dcc6ef01d2bd
tigju <tigju24@gmail.com>
2020-05-07 16:57:58 -0400

    Change module name

--- tigju_lambdata/my_script.py
+++ tigju_lambdata/my_script.py
@@ -1,6 +1,6 @@
 import pandas as pd
 #from my_func import enlarge
-from my_lambdata import my_func
+from tigju_lambdata import my_func
 # print('Hello')
 
 # df = pd.DataFrame({'a':[1,2,3], 'b':[4,5,6]})
