73dc90919ac1ac8bba01d7a6cfee5c5020392545
tigju <tigju24@gmail.com>
2020-05-08 21:36:42 -0400

    Adding method create_df to ConfusionMatrix class

--- tigju_lambdata/oop.py
+++ tigju_lambdata/oop.py
@@ -3,6 +3,18 @@ from sklearn.metrics import confusion_matrix
 from sklearn.utils.multiclass import unique_labels
 
 class ConfusionMatrix:
+
+    '''
+    This class creates and object of confusion matrix 
+    for classification predictions
+
+    Params: x_axis (series, true values)
+            y_axis (series, predicted values) 
+            title (string, name of the confusion matrix)
+            size (tuple, containing 2 integers of width and height of plot)
+
+    Returns: Object (confusion matrix)
+    '''
     def __init__(self, x_axis, y_axis, title, size=(10, 7)):
         self.x_axis = x_axis
         self.y_axis = y_axis
@@ -15,6 +27,14 @@ class ConfusionMatrix:
     def labels(self):
         return unique_labels(self.x_axis)
 
+    def create_df(self):
+        cm = self.cm()
+        cols = self.labels()
+        df = pd.DataFrame(cm, columns=cols, index=cols)
+        return df
+    # def plot_cm(self):
+
+
 
 if __name__ == "__main__":
     
@@ -23,4 +43,4 @@ if __name__ == "__main__":
 
     cm1 = ConfusionMatrix(y_true, y_pred, "Example", (6,4))
     
-    print(cm1.labels())
+    print(cm1.create_df())
